09ba3cf4871707f013176e1e6a2664543b607463
zhouqiang00 <zhouqiang00@tsinghua.org.cn>
2017-01-20 17:29:12 +0800

    Upload initial files of particle segmentation program

---
+++ config.h
@@ -0,0 +1,72 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `fftw3' library (-lfftw3). */
+#define HAVE_LIBFFTW3 1
+
+/* Define to 1 if you have the `fftw3f' library (-lfftw3f). */
+/* #undef HAVE_LIBFFTW3F */
+
+/* Define to 1 if you have the `fftw3f_threads' library (-lfftw3f_threads). */
+/* #undef HAVE_LIBFFTW3F_THREADS */
+
+/* Define to 1 if you have the `fftw3_threads' library (-lfftw3_threads). */
+#define HAVE_LIBFFTW3_THREADS 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Use MPI */
+#define HAVE_MPI 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "scheres@mrc-lmb.cam.ac.uk"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "relion"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "relion 1.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "relion"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
