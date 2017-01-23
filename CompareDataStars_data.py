b43a4bf0a65db0e42e58db2107c974c076d72f42
zhouqiang00 <zhouqiang00@tsinghua.org.cn>
2017-01-23 17:41:26 +0800

    Add files via upload

--- CompareDataStars_data.py
+++ CompareDataStars_data.py
@@ -94,24 +94,32 @@ def calculateEulerTransformation(e1,e2,e3,e4,e5,e6,type="spider",symmetry="c1"):
 def main():
 	prog_name=os.path.basename(sys.argv[0])
 	usage="""
-	# Compare Euler angle differences between different segmented parts of particles:
-	# Corresponding segmented particles have same Image Name
-	# data2.star is hierachied by Micrograph.
-	{prog} < --input_data_star1 data1.star> < --input_data_star2 data2.star  > < -o output star file > < --sameImageName >
+	# Compare Euler angles between two metadata files which are specified with --input_data_star1 and --input_data_star2.
+	# Corresponding particles in these two files must have the same image name. This is usually not the case.
+	# You can use following command to substitute the suffix of the data2.star to the suffix of data1.star to make them have the same image name. And then use the intermediate file temp_data2.star as --input_data_star2.
+	# cat data2.star | sed 's/suffixdata2/suffixdata1/g' > temp_data2.star
 
-	# substitue imageName of data1.star with imageName of data2.star. Particles have same coordinates @ same micrograph:
-	# data2.star is hierachied by Micrograph.
-	{prog} < --input_data_star1 data1.star> < --input_data_star2 data2.star  > < -o output star file > <--changename> < --sameCoordinate>
-	# Find rotating angle of volume of data1.star against to volume of data2.star:
-	{prog} < --input_data_star1 data1.star> < --input_data_star2 data2.star  > < -o output star file >  --calculateEulerTransform --sameImageName
-	#Transform euler angle of particles against to a rotated or flipped ref:
+	# Find Euler angle distance between two data.star files
+	# The fields in output file are:
+	# ImageName,Phi1,Theta1,Psi1,Phi2,Theta2,Psi2,deltaEuler,deltaPhi,deltaTheta,deltaPsi
+	# deltaEuler is the Euler angle distance of all of three Euler angles (phi, theta, psi).
+	# deltaPhi/Theta/Psi is calculated by algebraic subtraction of respective Euler angle.
+	{prog} < --input_data_star1 data1.star> < --input_data_star2 temp_data2.star  > < -o output file >  --calculateEulerError --sameImageName
+
+	# Find rotating angle of the reference volume of data1.star against to the reference volume of data2.star:
+	# The fields in output file are:
+	# ImageName, PhiET, ThetaET, PsiET, PhiET+PsiET, deltaPhi, deltaTheta, deltaPsi
+	# ET is the abbreation for Euler Transformation which is calculated by tranformation of Euler angle matrix.
+	# delta??? is calculated by algebraic subtraction.
+	{prog} < --input_data_star1 data1.star> < --input_data_star2 temp_data2.star  > < -o output file >  --calculateEulerTransform --sameImageName
+
+	# Transform euler angle of particles against to a rotated or flipped ref:
 	{prog} < --input_data_star1 data1.star> < -o output star file >  < --TransformEuler > < [ --target_rot --target_tilt --target_psi  ] | [ --target_emanaz --target_emanalt --target_emanphi ] | [ --flipX | --flipY | --flipZ ]>
 	""".format(prog=prog_name)
 	optParser= OptionParser(usage)
 	optParser.add_option("--input_data_star1",action="store",type="str",dest="input_data_star1",default="",help="Input relion data.star file #1. [default: %default]")
 	optParser.add_option("--input_data_star2",action="store",type="str",dest="input_data_star2",default="",help="Input relion data.star file #2, usually a reference data.star with pre-determined values. [default: %default]")
 	optParser.add_option("--sameImageName",action="store_true",dest="sameImageName",default=False,help="Two input data.star files have same image names for same particles. [default: %default]")
-	optParser.add_option("--sameCoordinate",action="store_true",dest="sameCoordinate",default=False,help="Two input data.star files have same coordinates and same micrographs for same particles. [default: %default]")
 	optParser.add_option("-o","--outputstar",action="store",type="str",dest="outputstar",default="",help="Output relion data.star file. [default: %default]")
 	optParser.add_option("--calculateTransError",action="store_true",dest="calculateTransError",default=False,help="Calculate Translation differences between two input data.star. [default: %default]")
 	optParser.add_option("--binningfactor1",action="store",type="float",dest="binningfactor1",default=1.0,help="Binning factor of input_data_star #1. [default: %default]")
@@ -133,8 +141,6 @@ def main():
 	optParser.add_option("--RandomEulerbySym",action="store_true",dest="RandomEulerbySym",default=False,help="Random Euler angle by symmetry. (Random choose a symmetry-related Euler angle for particles)'. [default: %default]")
 	optParser.add_option("--symmetry",action="store",type="str",dest="symmetry",default="C1",help="The symmetry used in the 3D reconstruction. [default: %default]")
 	#optParser.add_option("--Euleroff_Phi",action="store",type="float",dest="Euleroff_Phi",default=0.0,help="PHI of Euler angle of reference of input data.star 1 relative to that of input 2. [default: %default]")
-	optParser.add_option("--changename",action="store_true",dest="changename",default=False,help="Substitute ImageName of input data.star 1 with that of data.star 2 . [default: %default]")
-	optParser.add_option("--filter_NumberatStack",action="store",dest="filter_NumberatStack",default="",help="File used for filter input data.star 1 with ImageName of a format of number@stack. [default: %default]")
 	(options,args)=optParser.parse_args()
 	
 	data1=data_meta(options.input_data_star1)
@@ -164,18 +170,6 @@ def main():
 				allmics2[mic]=[]
 				allmics2[mic].append(data2.data_["datas"][i])
 	
-	if options.changename:
-		for record1 in data1.data_["datas"]:
-			mic = record1[i_MicrographName_1]
-			if allmics2.has_key(mic):
-				for record2 in allmics2[mic]:
-					if options.sameCoordinate:
-						if record1[i_CoordinateX_1]  == record2[i_CoordinateX_2] and record1[i_CoordinateY_1]  == record2[i_CoordinateY_2]:
-							record1[i_ImageName_1]  = record[i_ImageName_2]
-							break
-		if options.outputstar:
-			data1.write(options.outputstar)
-	
 
 	if options. calculateTransError:
 		i_x1,i_y1,i_sx1,i_sy1=getCoordOriginIndexes(data1.data_["labels"])
@@ -184,8 +178,6 @@ def main():
 			mic = record1[i_MicrographName_1]
 			if allmics2.has_key(mic):
 				for record2 in allmics2[mic]:
-					if options.sameCoordinate:
-						pass
 					if options.sameImageName:
 						if record1[i_ImageName_1]  == record2[i_ImageName_2] :
 							x1,y1,sx1,sy1=getCoordOriginFromRelionDataRecord(record1,i_x1,i_y1,i_sx1,i_sy1)
@@ -207,8 +199,6 @@ def main():
 			mic = record1[i_MicrographName_1]
 			if allmics2.has_key(mic):
 				for record2 in allmics2[mic]:
-					if options.sameCoordinate:
-						pass
 					if options.sameImageName:
 						if record1[i_ImageName_1]  == record2[i_ImageName_2] :
 							e1,e2,e3=getEulersFromRelionDataRecord(record1,i_rot1,i_tilt1,i_psi1)
@@ -333,8 +323,6 @@ def main():
 			mic = record1[i_MicrographName_1]
 			if allmics2.has_key(mic):
 				for record2 in allmics2[mic]:
-					if options.sameCoordinate:
-						pass
 					if options.sameImageName:
 						if record1[i_ImageName_1]  == record2[i_ImageName_2] :
 							e1,e2,e3=getEulersFromRelionDataRecord(record1,i_rot1,i_tilt1,i_psi1)
@@ -344,32 +332,8 @@ def main():
 							break
 		if options.outputstar:
 			f.close()
-		#print "The match records between two star file is %d particles"%(count)
-		#print "The average Euler angle difference between two input star files is: %f degrees"%(diff_eulers / count)
 		print "The fields in output file are:"
 		print "ImageName, PhiET, ThetaET, PsiET, PhiET+PsiET, deltaPhi, deltaTheta, deltaPsi"
-	if options.filter_NumberatStack :
-		# hierachy filter_NumberatStack files by image stack name and number
-		filter=open(options.filter_NumberatStack)
-		filter_record={}
-		for record in filter:
-			splrec=record.split()
-			number,img=splrec[0].split("@")
-			if filter_record.has_key(img):
-				filter_record[img].append(number)
-			else:
-				filter_record[img]=[number,]
-		filter.close()
-	
-		dummy=[]
-		for record1 in data1.data_["datas"]:
-			number,img = record1[i_ImageName_1].split("@")
-			if filter_record.has_key(img):
-				if number in filter_record[img]:
-					dummy.append(record1)
-
-		data1.data_["datas"]=dummy
-		data1.write(options.outputstar)
 		
 if __name__ == "__main__":
 	main()
