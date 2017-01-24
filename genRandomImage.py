72748942810f98e2a2cd8e814b518c41e450be44
zhouqiang00 <zhouqiang00@tsinghua.org.cn>
2017-01-24 14:00:13 +0800

    Add files via upload

--- genRandomImage.py
+++ genRandomImage.py
@@ -1,4 +1,27 @@
-#!/home/zhouqiang/softwares/EMAN2/extlib/bin/python
+#!/usr/local/EMAN2/extlib/bin/python
+#
+# Author: Qiang Zhou, 1/24/2017 (zhouqiang00@tsinghua.org.cn)
+# School of Life Sciences, Tsinghua University, Beijing, China
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# This complete copyright notice must be included in any revised version of the
+# source code. Additional authorship citations may be added, but existing
+# author citations must be preserved.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  2111-1307 USA
+
+
 from EMAN2 import *
 import os,sys
 import random
